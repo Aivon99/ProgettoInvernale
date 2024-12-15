ab4eaffc5ef7109f7fdb9ca6e37f04e39f3760e1
Aivon99 <109174292+Aivon99@users.noreply.github.com>
2024-12-15 20:02:11 +0100

    started folder

---
+++ Esercizio1.java
@@ -0,0 +1,33 @@
+/*
+Formato linea txt file> <linea> ::= <nomefile><spazio><dimensione> 
+
+Il programma riceve tre parametri da linea di comando:
+• il nome del file da cui acquisire i dati;
+• un percorso del file system (nello stesso formato di <nomefile>);
+• un'estensione (nello stesso formato di <nome>).
+
+E deve visualizzare in output la dimensione totale dei file che si trovano nel ramo del file system identificato
+dal percorso specificato, il cui nome termina con l'estensione passata come parametro, preceduta dal
+carattere ‘.’.
+
+Stampare i risultati ottenuti utilizzando due implementazioni distinte basate su strutture dati diverse (che,
+ovviamente, devono risultare uguali) prefissando i risultati dalle stringhe ‘Totale 1:’ e ‘Totale 2:’.
+
+Devono essere gestiti i seguenti errori:
+• file di input non valido;
+• parametri di input mancanti o malformati;
+• percorso o estensione non trovati.
+
+ */
+
+//Divisione: un metodo per la lettura, una classe per ciascun metodo di conteggio, un main 
+class Esercizio1{
+    public static void main(String args[]){
+
+        System.out.println("Ciao");
+        
+
+
+    }
+
+}
